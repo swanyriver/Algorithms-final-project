10ddfe27bd4d8858b74d12842d2a6c7149f62696
Brandon Swanson <swanyriver@gmail.com>
2015-12-02 00:59:53 -0800

    prim without heap implemented, inorder traversal gives 1.35 to 1.44 aproxamation, need to keep looking

--- tspaprox.py
+++ tspaprox.py
@@ -1,7 +1,11 @@
 import getcities
 import primm
+from math import sqrt
 
 import sys
+
+def distance(u,v):
+    return int(round(sqrt( (u.x-v.x)**2 + (u.y-v.y)**2 )))
 
 if len(sys.argv) < 2: 
   print "must supply input file"
@@ -13,8 +17,31 @@ cities = getcities.readCities(sys.argv[1], primm.primcity)
 
 root = primm.primmtree(cities)
 
+# for city in cities:
+#     if city == root: continue
+#     city.predecessor.descendants.append(city)
+
+
 for city in cities:
   print city
 
 
-#for city in cities
+stack = [root]
+tour = []
+
+while stack:
+    city = stack.pop()
+    tour.append(city)
+    stack.extend(city.descendants)
+
+totaldistance = 0
+#this will not scale well
+for i,j in zip(range(len(tour)), range(1,len(tour)) + [0]):
+    totaldistance += distance(tour[i],tour[j])
+    print i,":",tour[i].id
+
+print totaldistance
+
+
+
+

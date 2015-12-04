1b5a57b46999b8be46a9cc8f883b77eff1b3017a
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-12-03 20:30:51 -0800

    auto testing

--- solveall.sh
+++ solveall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-for f in ls provided/tsp_test_cases/test-input-*.txt;
+for f in $(ls provided/tsp_test_cases/test-input-*.txt);
 do
     echo $f
-    time python nearNBapprox.py $f 178
+    #time python nearNBapprox.py $f 178
 done    
 
-for f in ls provided/tsp_example_*.txt;
+for f in $(ls provided/tsp_example_*.txt);
 do
     echo $f
-   time python nearNBapprox.py $f 178
+   #time python nearNBapprox.py $f
 done 
 

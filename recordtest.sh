0dabe99b3dd88a958bd6e62e1952b30cae9fc435
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-12-03 20:54:40 -0800

    test with tee and more readable output

---
+++ recordtest.sh
@@ -0,0 +1,2 @@
+ ./solveall.sh 2>&1 | tee record.txt
+
--- solveall.sh
+++ solveall.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 for f in $(ls provided/tsp_test_cases/test-input-*.txt);
 do
-    echo $f
-    time python nearNBapprox.py $f 178
+    echo "---------------$f--------------------------------"
+    echo 
+    time python -u nearNBapprox.py $f 178
+    head -1 $f.tour
+    echo " "
+    echo "  "
+    echo "  "
 done    
 
 for f in $(ls provided/tsp_example_*.txt);
-do
-    echo $f
-   time python nearNBapprox.py $f
+do   
+    echo "---------------$f--------------------------------"
+    echo 
+    time python -u nearNBapprox.py $f
+    head -1 $f.tour
+    echo " "
+    echo "  "
+    echo "  "
 done 
 

2886d601be1ef128ab2dee9831c5bceb72f10bdb
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-12-03 20:31:49 -0800

    solve script corrected to not call with ls as argument

--- solveall.sh
+++ solveall.sh
@@ -2,12 +2,12 @@
 for f in $(ls provided/tsp_test_cases/test-input-*.txt);
 do
     echo $f
-    #time python nearNBapprox.py $f 178
+    time python nearNBapprox.py $f 178
 done    
 
 for f in $(ls provided/tsp_example_*.txt);
 do
     echo $f
-   #time python nearNBapprox.py $f
+   time python nearNBapprox.py $f
 done 
 
